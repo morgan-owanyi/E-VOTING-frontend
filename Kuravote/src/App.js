f84a74fbe4fa2d84412fffb510d12582595792df
Jordan <fetajordan6@gmail.com>
2025-12-05 12:04:07 +0300

    Frontend build

---
+++ Kuravote/src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import Vote from './pages/Vote';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/vote" element={<Vote />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
